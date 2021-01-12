1d2d9000e38f979f55ea421b13007c03d238bfbb
Lucas Wottrich <lucas.wottrich@warren.com.br>
2021-01-12 11:47:30 -0300

    new version 1.0.14

--- mylibrary/src/main/java/br/com/warren/mylibrary/Main.kt
+++ mylibrary/src/main/java/br/com/warren/mylibrary/Main.kt
@@ -1,13 +1,6 @@
 package br.com.warren.mylibrary
 
-import android.content.Context
-import android.widget.Toast
-import androidx.annotation.Keep
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-
-@Keep
-public class Main {
+class Main {
 
     fun classMain() {}
 
