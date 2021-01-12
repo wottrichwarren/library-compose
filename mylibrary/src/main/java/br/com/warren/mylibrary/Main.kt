385f45fc073d88ee3df892fb8637a702705ea0e4
Lucas Wottrich <lucas.wottrich@warren.com.br>
2021-01-12 09:36:46 -0300

    added library

---
+++ mylibrary/src/main/java/br/com/warren/mylibrary/Main.kt
@@ -0,0 +1,14 @@
+package br.com.warren.mylibrary
+
+import android.content.Context
+import android.widget.Toast
+import androidx.annotation.Keep
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Keep
+public class Main {
+
+    fun classMain() {}
+
+}
